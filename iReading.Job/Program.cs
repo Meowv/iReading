8202add2cc938184616385817d15321321c2751c
阿星Plus <hackxing@foxmail.com>
2017-09-15 09:26:45 +0800

    项目结构搭建初始化，工作之余对项目的构思。

---
+++ iReading.Job/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iReading.Job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
