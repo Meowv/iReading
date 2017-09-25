7fe8f2cde205be2c52d5d68dccb5abf0726982e7
阿星Plus <hackxing@foxmail.com>
2017-09-25 16:59:11 +0800

    完成爬取文章脚本程序

--- iReading.Job/Program.cs
+++ iReading.Job/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using iReading.DAL;
+using iReading.Entity;
+using iReading.Utility;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace iReading.Job
 {
@@ -6,7 +11,24 @@ namespace iReading.Job
     {
         static void Main(string[] args)
         {
-            
+            var url = "https://interface.meiriyiwen.com/article/random";
+            string json = HttpHelper.SendPost(url, new Dictionary<string, string>(), "get");
+            JObject obj = JObject.Parse(json);
+
+            using (ArticleDbContext db = new ArticleDbContext("Server=.;Database=Reading;User ID=sa;Password=123456"))
+            {
+                db.Articles.Add(new Articles()
+                {
+                    Title = (string)obj["data"]["title"],
+                    Author = (string)obj["data"]["author"],
+                    Details = (string)obj["data"]["content"],
+                    Summary = (string)obj["data"]["digest"],
+                    Category = "阅读",
+                    IssueTime = DateTime.Now,
+                    UpdateTime = DateTime.Now
+                });
+                db.SaveChanges();
+            }
         }
     }
 }
